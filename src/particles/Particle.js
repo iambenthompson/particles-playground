7c95dfed2c3c7e6915dd43dde768ee842194dbc7
Ben Thompson <git@iambenthompson.com>
2017-08-02 19:05:25 -0400

    Adds acceleration calc and config to Particle

--- src/particles/Particle.js
+++ src/particles/Particle.js
@@ -8,9 +8,15 @@ class Particle extends Point3D {
                 z = 0,
                 speedX = -0.3 + Math.random() * 0.6,
                 speedY = -0.3 + Math.random() * 0.6,
-                speedZ = -10.1 + Math.random() * 10.2
+                speedZ = -10.1 + Math.random() * 10.2,
+                accelerationX = 0,
+                accelerationY = 0,
+                accelerationZ = 0
               } = {}) {
     super({x, y, z});
+    this.accelerationX = accelerationX;
+    this.accelerationY = accelerationY;
+    this.accelerationZ = accelerationZ;
     this.speedX = speedX;
     this.speedY = speedY;
     this.speedZ = speedZ;
@@ -30,6 +36,10 @@ class Particle extends Point3D {
     this.x += this.speedX;
     this.y += this.speedY;
     this.z += this.speedZ;
+
+    this.speedX += this.accelerationX;
+    this.speedY += this.accelerationY;
+    this.speedZ += this.accelerationZ;
 
     this.age++
   }
