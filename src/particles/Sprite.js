a8c459ba9e19d73517bb872e7e6e3a23310c429f
Ben Thompson <git@iambenthompson.com>
2017-08-16 00:33:55 -0400

    Splits Sprite color into rgba

--- src/particles/Sprite.js
+++ src/particles/Sprite.js
@@ -2,7 +2,12 @@ import Particle from './Particle';
 
 class Sprite {
   constructor({
-                color = "#ffffff",
+                color = {
+                  red: 255,
+                  green: 255,
+                  blue: 255,
+                  alpha: 1
+                },
                 opacity = 1,
                 size = 1
               } = {}) {
@@ -17,13 +22,13 @@ class Sprite {
         particle
       } = {}){
     let drawnSize = this.size * perspective;
-    //let ageRatio = particle.age / particle.lifespan;
+    let ageRatio = particle.age / particle.lifespan;
     context.beginPath();
     context.arc(particle.x, particle.y, drawnSize, 0, 2 * Math.PI);
     context.closePath();
-    //this.context.fillStyle = 'rgba(' + particle.red + ', ' + particle.green + ', ' + particle.blue + ', ' + particle.opacity * (particle.lifespan - particle.age) / particle.lifespan + ')';
-    context.fillStyle = this.color;
+    context.fillStyle = 'rgba(' + this.color.red + ', ' + this.color.green + ', ' + this.color.blue + ', ' + this.color.alpha * (1-ageRatio) + ')';
     context.fill();
+
 //    this.context.strokeStyle = 'rgba(' + particle.red + ', ' + particle.green + ', ' + particle.blue + ', ' + particle.opacity * ageRatio + ')';
 //    this.context.lineWidth = this.STROKE_WIDTH_MAX * perspective;
 //    this.context.stroke();
